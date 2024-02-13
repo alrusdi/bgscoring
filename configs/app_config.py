c34bd8163e15b95c63c03fb5e8efa1cdee2fec69
Kai Letov <kai.letov@gmil.com>
2024-02-13 23:51:59 +0200

    Initial

---
+++ configs/app_config.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_NAME = os.environ.get("DB_NAME")
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
+DB_USER = os.environ.get("DB_USER")
+DB_PASS = os.environ.get("DB_PASS")
+
+DB_NAME_TEST = os.environ.get("DB_NAME_TEST")
+DB_HOST_TEST = os.environ.get("DB_HOST_TEST")
+DB_PORT_TEST = os.environ.get("DB_PORT_TEST")
+DB_USER_TEST = os.environ.get("DB_USER_TEST")
+DB_PASS_TEST = os.environ.get("DB_PASS_TEST")
+
+SECRET_AUTH = os.environ.get("SECRET_AUTH")
+
+REDIS_HOST = os.environ.get("REDIS_HOST")
+REDIS_PORT = os.environ.get("REDIS_PORT")
---
+++ tests/test_main.py
@@ -0,0 +1,10 @@
+# from fastapi.testclient import TestClient
+#
+# from src.main import bgs
+#
+#
+# def test_answer():
+#     client = TestClient(app=bgs)
+#     result = client.get("/")
+#     assert result.status_code == 200
+#     assert result.json() == {"answer": "Success"}
